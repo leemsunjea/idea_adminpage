fadde3b9b189b352996b8fd5c706ba8f0eb4f5f5
“leemsunjea” <leensunjea@gmail.com>
2025-09-05 10:13:23 +0900

    1.1.3
    수퍼베이스 환경변수 미설정으로 에러남

--- supabase_client.py
+++ supabase_client.py
@@ -5,5 +5,11 @@ from supabase import create_client, Client
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY") or os.getenv("SUPABASE_KEY")
 
+# 환경 변수가 없을 때 기본값 설정
+if not SUPABASE_URL:
+    SUPABASE_URL = "https://gkhdplnpkizhatxvvttn.supabase.co"
+if not SUPABASE_SERVICE_ROLE_KEY:
+    SUPABASE_SERVICE_ROLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdraGRwbG5wa2l6aGF0eHZ2dHRuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NTQ0MjE1NiwiZXhwIjoyMDcxMDE4MTU2fQ.-sTLezqfVWEj5NYLOtVEunDF5wsWTJbyspfPZrVDir4"
+
 # Supabase 클라이언트 초기화
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
